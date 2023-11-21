c3ad910e5756d7192d8ceb1f0e0b95ba51446b1d
Marco Escaleira <marcoescaleira2000@gmail.com>
2023-11-21 00:54:43 +0000

    testing further github actions and vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex w-screen min-h-screen items-center justify-center">
-      <h1 className='text-4xl text-blue-400'>Meet the countries2</h1>
+      <h1 className='text-4xl text-blue-400'>Meet the countries3</h1>
     </main>
   )
 }
