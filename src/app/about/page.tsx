acc958026f57cfd8d4e364645ead0c880d230ac1
Marco Escaleira <marcoescaleira2000@gmail.com>
2023-11-21 00:00:39 +0000

    playing around with app router

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex w-screen min-h-screen items-center justify-center">
-      <h1 className='text-4xl text-blue-400'>Meet the countries</h1>
+      <h1 className='text-4xl text-blue-400'>About page</h1>
     </main>
   )
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function Profile() {
   return (
     <main className="flex w-screen min-h-screen items-center justify-center">
-      <h1 className='text-4xl text-blue-400'>Meet the countries</h1>
+      <h1 className='text-4xl text-blue-400'>User profile</h1>
     </main>
   )
 }
