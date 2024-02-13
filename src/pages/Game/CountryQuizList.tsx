52c903c680921ef0160bdc20e4b5331a1ab74cf0
Marco Escaleira <marcoescaleira2000@gmail.com>
2024-02-13 23:15:01 +0000

    feat: style country quiz card

--- src/pages/Game/CountryQuizList.tsx
+++ src/pages/Game/CountryQuizList.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Typography } from "@material-tailwind/react";
+import { Button, Card, CardBody, CardFooter, CardHeader, IconButton, Typography } from "@material-tailwind/react";
 import { Loader2 } from "lucide-react";
 import { CountryQuizzesQuery } from "@generated/graphql.ts";
 import { CountryInfoModal } from "@pages/Game/CountryInfoModal.tsx";
@@ -19,7 +19,7 @@ export const CountryQuizList = ({ countryDetails, quizList, isLoadingCountryQuiz
   }
 
   const headerContent = (
-    <div className="mb-4 mt-8 flex items-center gap-8 border-b-2 pb-4">
+    <div className="px-2 md:px-0 mb-4 mt-8 flex items-center gap-8 border-b-2 pb-4">
       <Typography variant="h2" className="flex items-center gap-2 text-2xl">
         <img src={countryDetails.flags.svg} alt={countryDetails.name} className="h-5 w-5 rounded-full object-cover" />
 
@@ -41,10 +41,12 @@ export const CountryQuizList = ({ countryDetails, quizList, isLoadingCountryQuiz
   if (quizList.length === 0) {
     return (
       <>
-        {headerContent} <Typography className='mt-6'>No quizzes found for this country 🥹</Typography>
-        <div className="flex items-center mt-4">
+        {headerContent} <Typography className="mt-6">No quizzes found for this country 🥹</Typography>
+        <div className="mt-4 flex items-center">
           <Typography>Please try again later or&nbsp;</Typography>
-          <Button variant="text" className='px-1'>create your quiz</Button>
+          <Button variant="text" className="px-1">
+            create your quiz
+          </Button>
         </div>
       </>
     );
@@ -54,8 +56,28 @@ export const CountryQuizList = ({ countryDetails, quizList, isLoadingCountryQuiz
     <>
       {headerContent}
 
-      {quizList.map(({ title }) => (
-        <Card key={title}>{title}</Card>
+      {quizList.map(({ title, image, description }) => (
+        <Card key={title} className="w-full max-w-[20rem] shadow-lg">
+          <CardHeader floated={false} color="blue-gray" className='flex justify-center items-center pt-3'>
+            <img src={image} alt="the background for the quiz" className='h-40' />
+            <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+          </CardHeader>
+          <CardBody>
+            <div className="mb-3 flex items-center justify-start">
+              <Typography variant="h5" color="blue-gray" className="font-medium">
+                {title}
+              </Typography>
+            </div>
+            <Typography color="gray">
+              {description}
+            </Typography>
+          </CardBody>
+          <CardFooter className="">
+            <Button size="md" fullWidth={true}>
+              Start
+            </Button>
+          </CardFooter>
+        </Card>
       ))}
     </>
   );
