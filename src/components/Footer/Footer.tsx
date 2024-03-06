765e47e4711f610d3e306a4e3498e0e7deb32ffd
Marco Escaleira <marcoescaleira2000@gmail.com>
2024-03-06 21:59:50 +0000

    style: simple footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
+import { Typography } from "@material-tailwind/react";
+import { format } from "date-fns";
+
 export function Footer() {
   return (
-    <footer className="w-full bg-white p-8">
-
+    <footer className="flex w-full items-center justify-center gap-4 bg-white p-4">
+      <img src="/images/mtc-logo.svg" alt="Planet Earth" className="h-[34px] w-[38px] md:h-[44px] md:w-[48px]" />
+      <Typography color="gray">© {format(new Date(), "yyyy")} Meet The Countries</Typography>
     </footer>
   );
 }
