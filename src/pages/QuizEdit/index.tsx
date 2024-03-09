9febcec022322155930061724474323a92bb0631
Marco Escaleira <marcoescaleira2000@gmail.com>
2024-03-09 23:41:07 +0000

    style: quiz create form description

--- src/pages/QuizEdit/index.tsx
+++ src/pages/QuizEdit/index.tsx
@@ -18,6 +18,16 @@ export function Component() {
         <Typography variant="h2">{role === Roles.Admin ? "Create a new quiz" : "Submit a quiz"}</Typography>
       )}
 
+      {!quizId && (
+        <Typography className="my-4 w-3/5">
+          Create your own quiz and engage your audience with our easy-to-use quiz creation tool! Whether you&apos;re a
+          teacher, marketer, or just looking to have fun, our platform allows you to craft customized quizzes tailored
+          to your specific needs. Choose from a variety of question formats, single choice or multi choice. With our
+          intuitive interface, you&apos;ll have your quiz up and running in no time. Start creating and sharing your
+          quiz today!
+        </Typography>
+      )}
+
       <div className="mt-4 w-full py-1 md:w-3/5">
         <QuizForm />
       </div>
