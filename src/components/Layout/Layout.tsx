41529c5e9c8a4f350b5708a4c9dea0f8a8e7584d
Marco Escaleira <marcoescaleira2000@gmail.com>
2024-03-10 01:57:27 +0000

    feat: scroll to top on route change and scroll behavior to smooth

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 import { Outlet, useLocation } from "react-router-dom";
 import { Footer } from "@components/Footer/Footer.tsx";
 import { Header } from "@components/Header/Header.tsx";
+import { useScrollToTop } from "@utils/hooks/useScrollToTop";
 
 export function Layout() {
   const location = useLocation();
@@ -10,10 +11,12 @@ export function Layout() {
     Modal.setAppElement("#root");
   }, []);
 
+  useScrollToTop();
+
   return (
     <>
       <Header />
-      <main className="relative flex flex-grow w-full flex-col items-center">
+      <main className="relative flex w-full flex-grow flex-col items-center">
         <Outlet />
       </main>
       {location && location.pathname !== "/" && <Footer />}
